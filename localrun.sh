35b5ae86797a2751b89d5895bd790d9552d1dbf4
YaseenTa1 <yaseentahseen1@gmail.com>
2024-12-04 07:39:08 -0800

    Update localrun.sh

--- localrun.sh
+++ localrun.sh
@@ -7,10 +7,11 @@ docker run -d \
   -e TZ=Etc/UTC \
   -p 3000:3000 \
   -p 3001:3001 \
-  -v config/:/config \
+  -v /var/run/docker.sock:/var/run/docker.sock\
+  -v /root/casaos-in-docker/config/:/config \
   -v /sys/fs/cgroup:/sys/fs/cgroup:rw \
   --cgroupns=host \
   --privileged \
   --shm-size="1gb" \
   --restart unless-stopped \
-  casaos:local
+  casaos
